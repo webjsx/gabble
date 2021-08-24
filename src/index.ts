e27b55402ce73596bce3e1057b6062f200b764f0
Jeswin Kumar <jeswinpk@agilehead.com>
2021-08-24 07:23:28 +0530

    Update mkdirp import

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import yargs from "yargs";
 import beautify from "js-beautify";
 import * as path from "path";
 import * as fs from "fs";
-import * as mkdirp from "mkdirp";
+import mkdirp from "mkdirp";
 
 const argv = yargs(process.argv.slice(2))
   .scriptName("gabble")
